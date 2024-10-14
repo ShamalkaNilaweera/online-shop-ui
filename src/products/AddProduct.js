ece40e2a442996ef01e0043591d68ea545ed8d81
ShamalkaNilaweera <kavindi.nilaweera@gmail.com>
2024-10-14 14:27:31 +0530

    Populated the drop down

--- src/products/AddProduct.js
+++ src/products/AddProduct.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import AxiosGetRequest from './AxiosGetRequest';
 
 export default function AddProduct() {
     let navigate=useNavigate();
@@ -30,6 +31,35 @@ export default function AddProduct() {
         }
     }
 
+        const [options, setOptions] = useState([]);
+      
+        useEffect(() => {
+          async function fetchData() {
+            // Fetch data
+            const { data } = await axios.get("http://localhost:8081/api/v1/colors/");
+            const results = []
+            // Store results in the results array
+            data.forEach((value) => {
+              results.push({
+                key: value.color,
+                value: value.colorId,
+              });
+        
+            });
+            // Update the options state
+            setOptions([
+              {key: 'Select a color', value: ''}, 
+              ...results
+            ])
+          }
+      
+          // Trigger the fetch
+          fetchData();
+        }, []);
+    
+        // const { label, name1, ...rest } = props;
+    
+
   return (
     <div className='container'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
@@ -46,11 +76,19 @@ export default function AddProduct() {
                 <input type='{text}' className='form-control' placeholder='Enter Material' name='material' value={material} onChange={(event)=>onInputChange(event)}></input>
 
                 <label htmlFor='Size' className='form-label'>Item Size</label>
-                <input type='number' className='form-control' placeholder='Enter Item Size' name='size' value={size} onChange={(event)=>onInputChange(event)}></input>
+                <input type='{text}' className='form-control' placeholder='Enter Item Size' name='size' value={size} onChange={(event)=>onInputChange(event)}></input>
 
                 <label htmlFor='Color' className='form-label'>Item Color</label>
-                <input type='text' className='form-control' placeholder='Enter Item Color' name='color' value={color} onChange={(event)=>onInputChange(event)}></input>
-
+                <select className='form-control' placeholder='Select Item Color' name='color' value={color} onChange={(event)=>onInputChange(event)} >
+        {options.map((option) => {
+          return (
+            <option key={option.value} value={option.value}>
+              {option.key}
+            </option>
+          );
+        })}
+        </select>
+    
                 <label htmlFor='Quantity' className='form-label'>Item Quantity</label>
                 <input type='number' className='form-control' placeholder='Enter Item Quantity' name='quantity'  value={quantity} onChange={(event)=>onInputChange(event)}></input>
             </div>
